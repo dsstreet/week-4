05faba65eef47aca618c9caf8374df60c3070ae2
Andrew Street <astree5@live.spcollege.edu>
2026-02-15 23:27:44 -0500

    Completed Ex 8_1

---
+++ Ch08Ex1GuitarShop/GuitarShop/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using GuitarShop.Models;
+
+namespace GuitarShop.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private ShopContext context;
+        private List<Category> categories;
+
+        public ProductController(ShopContext ctx)
+        {
+            context = ctx;
+            categories = context.Categories
+                    .OrderBy(c => c.CategoryID)
+                    .ToList();
+        }
+
+        public IActionResult Index()
+        {
+            return RedirectToAction("List");
+        }
+
+        [Route("[area]/[controller]s/{id?}")]
+        public IActionResult List(string id = "All")
+        {
+            List<Product> products;
+            if (id == "All")
+            {
+                products = context.Products
+                    .OrderBy(p => p.ProductID).ToList();
+            }
+            else
+            {
+                products = context.Products
+                    .Where(p => p.Category.Name == id)
+                    .OrderBy(p => p.ProductID).ToList();
+            }
+
+            //create and populate view model
+            var model = new ProductListViewModel
+            {
+                Categories = categories,
+                Products = products,
+                SelectedCategory = id
+            };
+
+            // bind model to view
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            // create new Product object
+            Product product = new Product();                
+
+            // use ViewBag to pass action and category data to view
+            ViewBag.Action = "Add";
+            ViewBag.Categories = categories;
+
+            // bind product to AddUpdate view
+            return View("AddUpdate", product);
+        }
+
+        [HttpGet]
+        public IActionResult Update(int id)
+        {
+            // get Product object for specified primary key
+            Product product = context.Products
+                .Include(p => p.Category)
+                .FirstOrDefault(p => p.ProductID == id) ?? new Product();
+
+            // use ViewBag to pass action and category data to view
+            ViewBag.Action = "Update";
+            ViewBag.Categories = categories;
+
+            // bind product to AddUpdate view
+            return View("AddUpdate", product);
+        }
+
+        [HttpPost]
+        public IActionResult Update(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                bool isNew = product.ProductID == 0;
+
+                if (isNew)
+                    context.Products.Add(product);
+                else
+                    context.Products.Update(product);
+
+                context.SaveChanges();
+
+                TempData["UserMessage"] = isNew
+                    ? $"You just added the product {product.Name}"
+                    : $"You just updated the product {product.Name}";
+
+                return RedirectToAction("List");
+            }
+            else
+            {
+                ViewBag.Action = "Save";
+                ViewBag.Categories = categories;
+                return View("AddUpdate", product);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            Product product = context.Products
+                .FirstOrDefault(p => p.ProductID == id) ?? new Product();
+            return View(product);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Product product)
+        {
+            context.Products.Remove(product);
+            context.SaveChanges();
+            return RedirectToAction("List");
+        }
+    }
+}

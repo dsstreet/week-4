2749f8cbae15cec4fabe666d48ff53cfb6f111f5
Andrew Street <astree5@live.spcollege.edu>
2026-02-15 21:25:51 -0500

    Completed Exercise 7-1

--- GuitarShop/Controllers/ProductController.cs
+++ GuitarShop/Controllers/ProductController.cs
@@ -31,6 +31,14 @@ namespace GuitarShop.Controllers
                 products = context.Products
                     .OrderBy(p => p.ProductID).ToList();
             }
+            else if (id == "Strings")
+            { 
+                products = context.Products
+                    .Where(p => p.Category.Name == "Guitars"
+                            || p.Category.Name == "Basses")
+                    .OrderBy(p => p.ProductID)
+                    .ToList();
+            }
             else
             {
                 products = context.Products
@@ -38,9 +46,14 @@ namespace GuitarShop.Controllers
                     .OrderBy(p => p.ProductID).ToList();
             }
 
-            // use ViewBag to pass data to view
-            ViewBag.Categories = categories;
+                // use ViewBag to pass data to view
+                ViewBag.Categories = categories;
             ViewBag.SelectedCategoryName = id;
+
+            if (id == "Strings")
+            {
+                ViewBag.SelectedCategoryName = "Strings";
+            }
 
             // bind products to view
             return View(products);
